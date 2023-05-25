c80cf78e6f0f167f506cd6d050b80bc45037ebde
dongcheol <ehdcjf4874@daum.net>
2023-05-25 15:42:06 +0900

    [Feat] google map api로 변경

--- d2_main.js
+++ d2_main.js
@@ -54,43 +54,96 @@ const secondlog = new Chart("secondlog",{
     }
 );
 
-function addMarker(latitude, longitude) {
-  var map = L.map('map').setView([latitude, longitude], 13);
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    center: { lat: 37, lng: 150 },
+  });
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-  }).addTo(map);
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+  });
 
-  L.marker([latitude, longitude]).addTo(map);
+  // Add a marker clusterer to manage the markers.
+  new markerClusterer.MarkerClusterer({ map, markers });
+  
 }
 
-function getLocation() {
-    const url = `/location`;
+const locations = [
+  // Marker positions (json파일로 받아올 것)
+  { lat: -31.56391, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+  { lat: -34.671264, lng: 150.863657 },
+  { lat: -35.304724, lng: 148.662905 },
+  { lat: -36.817685, lng: 175.699196 },
+  { lat: -36.828611, lng: 175.790222 },
+  { lat: -37.75, lng: 145.116667 },
+  { lat: -37.759859, lng: 145.128708 },
+  { lat: -37.765015, lng: 145.133858 },
+  { lat: -37.770104, lng: 145.143299 },
+  { lat: -37.7737, lng: 145.145187 },
+  { lat: -37.774785, lng: 145.137978 },
+  { lat: -37.819616, lng: 144.968119 },
+  { lat: -38.330766, lng: 144.695692 },
+  { lat: -39.927193, lng: 175.053218 },
+  { lat: -41.330162, lng: 174.865694 },
+  { lat: -42.734358, lng: 147.439506 },
+  { lat: -42.734358, lng: 147.501315 },
+  { lat: -42.735258, lng: 147.438 },
+  { lat: -43.999792, lng: 170.463352 },
+];
+
+initMap();
+
+
+// 아이피 주소를 위,경도로 변환
+// function getLocation() {
+//     const url = `/location`;
   
-    // API 요청 보내기
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          //console.log(response.json())
-          return response.json();
-        } else {
-          throw new Error('Request failed. Status:', response.status);
-        }
-      })
-      .then(data => {
-        const latitude = data.location.latitude;
-        const longitude = data.location.longitude;
-        //console.log(latitude); // 위도 조회
-        //console.log(longitude); // 경도 조회
-        addMarker(latitude, longitude); // 위도와 경도를 사용하여 마커 추가
-        // TODO: 위도, 경도를 사용하여 리프레 맵에 마커를 찍는 등의 작업을 수행할 수 있습니다.
-      })
-      .catch(error => {
-        console.error('Request failed:', error.message);
-      });
-  }
+//     // API 요청 보내기
+//     fetch(url)
+//       .then(response => {
+//         if (response.ok) {
+//           //console.log(response.json())
+//           return response.json();
+//         } else {
+//           throw new Error('Request failed. Status:', response.status);
+//         }
+//       })
+//       .then(data => {
+//         const latitude = data.location.latitude;
+//         const longitude = data.location.longitude;
+//         //console.log(latitude); // 위도 조회
+//         //console.log(longitude); // 경도 조회
+//         addMarker(latitude, longitude); // 위도와 경도를 사용하여 마커 추가
+//         // TODO: 위도, 경도를 사용하여 리프레 맵에 마커를 찍는 등의 작업을 수행할 수 있습니다.
+//       })
+//       .catch(error => {
+//         console.error('Request failed:', error.message);
+//       });
+//   }
   
-getLocation();
+// getLocation();
 
   
