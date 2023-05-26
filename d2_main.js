dc3165348fb080f8e9a875590a7d46b738de427b
kjw <job.jiwon.2022@gmail.com>
2023-05-26 13:58:58 +0900

    [Feat]D2 메인 항목 전반적 수정

--- d2_main.js
+++ d2_main.js
@@ -1,58 +1,105 @@
-const fitstValue = [50,60,70,80,90,100,110,120]; //ALL영역에서 차단 IP개수를 넣기
+const firstValue = [50,60,70,80,90,100,110,120]; //ALL영역에서 차단 IP개수를 넣기
 const firstLabel = [7,8,8,9,9,9,10,11]; //차단된 IP이름을 넣고
 
 const secondValue = [50,60,70,80,90,100,110,120];
 const secondLabel = [7,8,8,9,9,9,10,11];
 
-const firstlog = new Chart("firstlog",{
-    type: "doughnut",
-    data: {
-        labels: firstLabel,
-          datasets: [{
-            label: 'My First Dataset',
-            data: fitstValue,
-            backgroundColor: [
-                'rgba(255, 67, 67, 1)',
-                'rgba(209, 51, 51, 1)',
-                'rgba(0, 114, 198, 1)',
-                'rgba(0, 182, 228, 1)',
-                'rgba(0, 163, 0, 1)',
-                'rgba(89, 197, 164, 1)',
-                'rgba(218, 247, 235, 1)',
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
-          }]
-        }
-    }
-);
+const backColor = [
+  'rgba(255, 67, 67, 1)',
+  'rgba(209, 51, 51, 1)',
+  'rgba(0, 114, 198, 1)',
+  'rgba(0, 182, 228, 1)',
+  'rgba(0, 163, 0, 1)',
+  'rgba(89, 197, 164, 1)',
+  'rgba(218, 247, 235, 1)',
+  'rgb(255, 99, 132)',
+  'rgb(54, 162, 235)',
+  'rgb(255, 205, 86)'
+];
 
-const secondlog = new Chart("secondlog",{
-    type: "doughnut",
-    data: {
-        labels: secondLabel,
-          datasets: [{
-            label: 'My First Dataset',
-            data: secondValue,
-            backgroundColor: [
-                'rgba(255, 67, 67, 1)',
-                'rgba(209, 51, 51, 1)',
-                'rgba(0, 114, 198, 1)',
-                'rgba(0, 182, 228, 1)',
-                'rgba(0, 163, 0, 1)',
-                'rgba(89, 197, 164, 1)',
-                'rgba(218, 247, 235, 1)',
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
-          }]
-        }
+
+const firstLogChart = document.getElementById('firstLogChart');
+const secondLogChart = document.getElementById('secondLogChart');
+
+new Chart(firstLogChart, {
+type: 'doughnut',
+data:{
+  labels : firstLabel,
+  datasets:[
+    {
+      label : 'IP',
+      data : firstValue.sort(),
+      backgroundColor : backColor,
+      hoverOffset : 4
+      }]
+    },
+    options :{
+      legend : {
+        display : true,
+        position : 'right',
+        align :'start',
+        fullWidth : false,
+      },
+      responsive : false
     }
-);
+    });
+
+
+new Chart(secondLogChart, {
+type: 'doughnut',
+data:{
+  labels : secondLabel,
+  datasets:[
+    {
+      label : 'RuleSet',
+      data : secondValue,
+      backgroundColor :backColor,
+      hoverOffset : 4
+      }]
+    },
+    options :{
+      legend : {
+        display : true,
+        position : 'right',
+        align :'start',
+        fullWidth : false,
+      },
+      responsive : false
+    }
+    });
+
+const high = document.getElementById('high');
+const mid = document.getElementById('mid');
+const low = document.getElementById('low');
+
+var dataGd = {
+  test1 : 1,
+  test2 : 2,
+  severity : 3
+}
+
+function severityData(dataGd){
+  var highGradeNum, midGradeNum, lowGradeNum = 0;
+  var gradeData = { 'high' : highGradeNum, 'mid':midGradeNum, 'low':lowGradeNum};
+  if (dataGd.severity > 6.9 && dataGd.severity < 9.0){
+    highGradeNum = highGradeNum + 1;
+    return gradeData['high'];
+  }
+  else if (dataGd.severity > 3.9 && dataGd.severity < 7.0){
+    midGradeNum = midGradeNum + 1;
+    return gradeData['mid'];
+  }
+  else if (dataGd.severity> 0.9 && dataGd.severity < 4.0){
+    lowGradeNum = lowGradeNum +1;
+    return gradeData['low'];
+  }
+
+  high.innerHTML = gradeData['high'];
+  mid.innerHTML = gradeData['mid'];
+  low.innerHTML = gradeData['low'];
+}
+
+severityData(dataGd);
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
