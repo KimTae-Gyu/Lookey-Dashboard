fd2a1cebee9854c8b1970095500759c17bb436cc
dongcheol <ehdcjf4874@daum.net>
2023-05-30 18:09:39 +0900

    [Feat] 메인페이지 경고창

--- d2_main.js
+++ d2_main.js
@@ -1,96 +1,220 @@
-const fitstValue = [50,60,70,80,90,100,110,120]; //ALL영역에서 차단 IP개수를 넣기
-const firstLabel = [7,8,8,9,9,9,10,11]; //차단된 IP이름을 넣고
+function showModal(message) {
+  // 모달 요소 가져오기
+  const modalContainer = document.getElementById('modalContainer');
+  const modalMessage = document.getElementById('modalMessage');
+  const modalCloseButton = document.getElementById('modalCloseButton');
 
-const secondValue = [50,60,70,80,90,100,110,120];
-const secondLabel = [7,8,8,9,9,9,10,11];
+  // // 모달 내용 설정
+  modalMessage.textContent = message;
 
-const firstlog = new Chart("firstlog",{
-    type: "doughnut",
-    data: {
-        labels: firstLabel,
-          datasets: [{
-            label: 'My First Dataset',
-            data: fitstValue,
-            backgroundColor: [
-                'rgba(255, 67, 67, 1)',
-                'rgba(209, 51, 51, 1)',
-                'rgba(0, 114, 198, 1)',
-                'rgba(0, 182, 228, 1)',
-                'rgba(0, 163, 0, 1)',
-                'rgba(89, 197, 164, 1)',
-                'rgba(218, 247, 235, 1)',
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
-          }]
-        }
-    }
-);
+  // 모달 보이기
+  modalContainer.style.display = 'block';
 
-const secondlog = new Chart("secondlog",{
-    type: "doughnut",
-    data: {
-        labels: secondLabel,
-          datasets: [{
-            label: 'My First Dataset',
-            data: secondValue,
-            backgroundColor: [
-                'rgba(255, 67, 67, 1)',
-                'rgba(209, 51, 51, 1)',
-                'rgba(0, 114, 198, 1)',
-                'rgba(0, 182, 228, 1)',
-                'rgba(0, 163, 0, 1)',
-                'rgba(89, 197, 164, 1)',
-                'rgba(218, 247, 235, 1)',
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
-          }]
-        }
-    }
-);
-
-function addMarker(latitude, longitude) {
-  var map = L.map('map').setView([latitude, longitude], 13);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-  }).addTo(map);
-
-  L.marker([latitude, longitude]).addTo(map);
+  // 닫기 버튼 클릭 이벤트 핸들러
+  modalCloseButton.addEventListener('click', () => {
+    // 모달 숨기기
+    modalContainer.style.display = 'none';
+  });
 }
 
-function getLocation() {
-    const url = `/location`;
-  
-    // API 요청 보내기
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          //console.log(response.json())
-          return response.json();
-        } else {
-          throw new Error('Request failed. Status:', response.status);
-        }
-      })
-      .then(data => {
-        const latitude = data.location.latitude;
-        const longitude = data.location.longitude;
-        //console.log(latitude); // 위도 조회
-        //console.log(longitude); // 경도 조회
-        addMarker(latitude, longitude); // 위도와 경도를 사용하여 마커 추가
-        // TODO: 위도, 경도를 사용하여 리프레 맵에 마커를 찍는 등의 작업을 수행할 수 있습니다.
-      })
-      .catch(error => {
-        console.error('Request failed:', error.message);
-      });
-  }
-  
-getLocation();
+showModal('침입이 탐지되었습니다.');
 
-  
+// 차트 렌더링 함수
+function renderChart() {
+  // 차트에서 사용될 색깔 정의
+  const backColor = [
+    'rgba(255, 67, 67, 1)',
+    'rgba(209, 51, 51, 1)',
+    'rgba(0, 114, 198, 1)',
+    'rgba(0, 182, 228, 1)',
+    'rgba(0, 163, 0, 1)',
+    'rgba(89, 197, 164, 1)',
+    'rgba(218, 247, 235, 1)',
+    'rgb(255, 99, 132)',
+    'rgb(54, 162, 235)',
+    'rgb(255, 205, 86)'
+  ];
+  // 1번째 차트 => NFW => IP ** NFW 그룹 바이한 결과를 서버에 요청해서 받는 코드 작성해야함.
+  // 서버에 NFW 그룹 바이한 결과를 요청해서 받아오는 코드
+  const firstValue = [50, 60, 70, 80, 90, 100, 110, 120]; //ALL영역에서 차단 IP개수를 넣기 -> group by
+  const firstLabel = [7, 8, 8, 9, 9, 9, 10, 11]; //차단된 IP이름을 넣고
+  const firstLogChart = document.getElementById('firstLogChart');
+
+  new Chart(firstLogChart, {
+    type: 'doughnut',
+    data: {
+      labels: firstLabel,
+      datasets: [
+        {
+          label: 'IP',
+          data: firstValue.sort(),
+          backgroundColor: backColor,
+          hoverOffset: 4
+        }]
+    },
+    options: {
+      legend: {
+        display: true,
+        position: 'right',
+        align: 'start',
+        fullWidth: false,
+      },
+      responsive: false
+    }
+  });
+  // fetch('http://localhost:3000/log/nfwChart')
+  //   .then(data => {
+
+  //   })
+  //   .catch(error => {
+  //     console.error('MainPage NFW Chart Error: ', error);
+  //   });
+
+
+
+  // 서버에 WAF 그룹 바이한 결과를 요청해서 받아오는 코드
+  fetch('http://localhost:3000/log/wafChart')
+    .then(response => response.json())
+    .then(data => {
+      let secondValue = [];
+      let secondLabel = [];
+      let ruleSet;
+
+      data.forEach(wafRuleSet => {
+        secondValue.push(wafRuleSet.count);
+        ruleSet = wafRuleSet._id.name.split(':');
+        secondLabel.push(ruleSet[ruleSet.length - 2]);
+      });
+
+      // // 2번째 차트 => WAF => Rule Set
+      // const secondValue = [50,60,70,80,90,100,110,120]; // 룰셋 별 막은 횟수 -> group by 해야될듯?
+      // const secondLabel = [7,8,8,9,9,9,10,11]; // 차단 룰셋 이름      
+      const secondLogChart = document.getElementById('secondLogChart');
+
+      new Chart(secondLogChart, {
+        type: 'doughnut',
+        data: {
+          labels: secondLabel,
+          datasets: [
+            {
+              label: 'RuleSet',
+              data: secondValue,
+              backgroundColor: backColor,
+              hoverOffset: 4
+            }]
+        },
+        options: {
+          legend: {
+            display: true,
+            position: 'right',
+            align: 'start',
+            fullWidth: false,
+          },
+          responsive: false
+        }
+      });
+    })
+    .catch(error => {
+      console.error('MainPage WAF Chart Error: ', error);
+    });
+}
+renderChart();
+
+// GuardDuty    
+const high = document.getElementById('high');
+const mid = document.getElementById('mid');
+const low = document.getElementById('low');
+
+var dataGd = {
+  test1: 1,
+  test2: 2,
+  severity: 3
+}
+
+function severityData(dataGd) {
+  var highGradeNum, midGradeNum, lowGradeNum = 0;
+  var gradeData = { 'high': highGradeNum, 'mid': midGradeNum, 'low': lowGradeNum };
+  if (dataGd.severity > 6.9 && dataGd.severity < 9.0) {
+    highGradeNum = highGradeNum + 1;
+    return gradeData['high'];
+  }
+  else if (dataGd.severity > 3.9 && dataGd.severity < 7.0) {
+    midGradeNum = midGradeNum + 1;
+    return gradeData['mid'];
+  }
+  else if (dataGd.severity > 0.9 && dataGd.severity < 4.0) {
+    lowGradeNum = lowGradeNum + 1;
+    return gradeData['low'];
+  }
+
+  high.innerHTML = gradeData['high'];
+  mid.innerHTML = gradeData['mid'];
+  low.innerHTML = gradeData['low'];
+}
+
+severityData(dataGd);
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    center: { lat: 37, lng: 150 },
+    minZoom: 2,
+    restriction: {
+      latLngBounds: {
+        north: 90,
+        south: -90,
+        east: 240,
+        west: 40,
+      },
+    },
+  });
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
+
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+  });
+
+  // Add a marker clusterer to manage the markers.
+  new markerClusterer.MarkerClusterer({ map, markers });
+
+}
+
+// // json 파일 불러와서 locations에 저장
+// fetch('./locations_json')
+//   .then(response => response.json())
+//   .then(data => {
+//     // data 변수에 JSON 데이터가 저장됨
+//     // 이후에 필요한 작업을 수행합니다
+//     console.log(data); // 예시: 데이터 출력
+//     const locations = data;
+//   })
+//   .catch(error => {
+//     // 오류 처리
+//     console.error('location json Error:', error);
+//   });
+
+const locations = [
+  // Marker positions (json파일로 받아올 것)
+  { lat: -31.56391, lng: 147.154312 }
+];
+
+initMap();
+
+window.showModal = showModal;
