caa63ce69aa3d3be9acdee2ed300cda937e6916a
Kim Tae-Gyu <zmfntpdlxj@naver.com>
2023-06-05 16:40:35 +0900

    [Fix] 오타 수정

--- mongo.js
+++ mongo.js
@@ -31,7 +31,7 @@ function nfwMongoInsert(connection, logs) {
 
 function mongoWafGroupBy(connection) {
   // 컬렉션 이름
-  const collectionName = 'waf';
+  const collectionName = 'wafs2';
   const collection = connection.collection(collectionName);
   // labels 필드로 그룹핑해서 카운트 상위 5개만 반환
   return collection.aggregate([
